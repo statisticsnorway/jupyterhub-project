442be1574b81f45d61b97667366790e7eeb63d5d
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2021-08-26 13:46:50 +0200

    Fixed proper import of spark module

--- docker/jupyterlab/pyspark_local/init.py
+++ docker/jupyterlab/pyspark_local/init.py
@@ -1,5 +1,6 @@
 # This will trigger initialization of Pyspark
-from pyspark import shell
+from pyspark.shell import *
 # Fix the Spark UI link
+from pyspark import SparkContext
 from dapla.spark.sparkui import uiWebUrl
 SparkContext.uiWebUrl = property(uiWebUrl)
