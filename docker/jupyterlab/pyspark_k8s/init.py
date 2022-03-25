11ee8d39d494ba9ebc3acd7db7ea4cd33df59a03
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2022-03-25 08:52:12 +0100

    Revert "Fix hostname for k8s kernel"
    This reverts commit f76c04bd7b2cd732612a2c27ac0af65438234792.

--- docker/jupyterlab/pyspark_k8s/init.py
+++ docker/jupyterlab/pyspark_k8s/init.py
@@ -9,6 +9,11 @@ from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
 from dapla.spark.sparkui import uiWebUrl
 
+# Get the local ip.
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect((os.environ.get("KUBERNETES_SERVICE_HOST", "8.8.8.8"), 80))
+local_ip = s.getsockname()[0]
+s.close()
 
 randomId = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
 appName = os.environ["JUPYTERHUB_CLIENT_ID"] + '-' + randomId
@@ -19,7 +24,7 @@ SparkContext._ensure_initialized()  # type: ignore
 try:
     spark = SparkSession.builder.appName(appName) \
         .config('spark.submit.deployMode', 'client') \
-        .config('spark.driver.host', os.environ["HOSTNAME"]) \
+        .config('spark.driver.host', local_ip) \
         .config('spark.executorEnv.JUPYTERHUB_API_TOKEN', os.environ["JUPYTERHUB_API_TOKEN"]) \
         .config('spark.kubernetes.driver.pod.name', os.environ["HOSTNAME"]) \
         .getOrCreate()
