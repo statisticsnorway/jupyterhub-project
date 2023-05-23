1f02d879140ebaa65e6825c83efbd5fa6265b29c
Anders Lunde <anders-lunde@hotmail.com>
2023-05-23 22:16:32 +0200

    Add validation for gitconfig

--- docker/jupyterlab/check-git-config.bash
+++ docker/jupyterlab/check-git-config.bash
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
+VALID_GIT_CONFIG="$(python -c 'from kvakk_git_tools import validate_git_config; print(validate_git_config())' 2>&1)"
 
-GIT_CONFIG=~/.gitconfig
-
-if [ ! -f $GIT_CONFIG ]; then
+# If the output of the command is anything else than
+# True we should run ssb_gitconfig.py ask the user to run ssb_gitconfig.py
+if [ "$VALID_GIT_CONFIG" != "True" ]; then
   cat << EOF
 Your Git account is not configured.
-
 To configure:
-
-  run: ssb-gitconfig.py
-
+  run: ssb_gitconfig.py
 EOF
 fi
